5efc3d43d37ecf54d58dc7549ca3145b8bc5a8ed
Otávio Carvalho <omcarvalho@inf.ufrgs.br>
2014-11-30 22:19:43 -0200

    Modificações na ordem do commit na criação da transação

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, session
+from flask import Flask
 import locale
 import os
 
@@ -9,10 +9,7 @@ app = Flask("app")
 app.secret_key = os.urandom(24)
 
 from app.presentation import home_view
-from app.presentation import login_view
 from app.presentation import user_view
-from app.presentation import subscribe_view
-from app.presentation import complaint_view
 from app.presentation import search_view
 from app.presentation import item_view
 
@@ -21,5 +18,3 @@ from app.presentation import transaction_select_view
 from app.presentation import transaction_exchange_view
 from app.presentation import transaction_create_view
 from app.presentation import transaction_accept_view
-
-from app.presentation import profile_view
